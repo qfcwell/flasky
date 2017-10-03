460c842ee85f6aea523a3924483ed118f1837955
qfcwell <qfcqfcqfc@qq.com>
2017-10-03 11:02:52 +0800

    a1

--- config.py
+++ config.py
@@ -9,8 +9,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     MAIL_SERVER = 'smtp.minitech.site'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    #MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
